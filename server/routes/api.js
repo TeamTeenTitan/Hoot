a118fe4b69edb50e03b4b31693706afc462f46e8
Patrick Reid <patrickjreid@gmail.com>
2022-04-15 14:36:19 -0600

    debugged regenerator runtime issues

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,22 +5,22 @@ const testController = require('../controllers/testController');
 
 const router = express.Router();
 
-//fetch top breaking news, order them, send back
-// router.get('/', testController.getNews, (req, res, next) => {
-//   res.status(200).json(res.locals.articles)
-//   // console.log('This is my get request on server.js')
-//   // res.send('Test')
-  
-// });
+// fetch top breaking news, order them, send back
+router.get('/', testController.getNews, (req, res, next) => {
+  res.status(200).json(res.locals.articles)
+  // console.log('This is my get request on server.js')
+  // res.send('Test')
+  // return next
+});
 
 
-router.get('/',
-  newsController.breakingNews,
-  // newsController.sortNews,
-  (req, res) => {
-    res.status(200).json(res.locals.articles)
-  }
-);
+// router.get('/',
+//   newsController.breakingNews,
+//   // newsController.sortNews,
+//   (req, res) => {
+//     res.status(200).json(res.locals.articles)
+//   }
+// );
 
 // //fetch news based on search, order them, send back
 // router.post('/search',
